2ecb2d29c7e3a3b190c4c7c7f63c6c5d889390c5
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-23 22:34:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,6 +6,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season, detail_hash|
     detail_hash.each do |key, value|
       if key[:status] == winner
+        first_name = [:name]
         binding.pry
       end
     end
