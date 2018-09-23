5f326f8a66174f0080ff6e1f904d87f8ebaacde9
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-23 23:30:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,16 +48,17 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  num_of_cont = 0
-  age_total = 0
-  data.each do |season_num, contestants|
-    if season_num == season
-      contestants.each do |cont_hash|
-        num_of_cont += 1
-        age_total += cont_hash["age"].to_i
+  no_of_cont = 0
+  tot_age = 0
+  data.each do |season_no, contestants|
+    if season_no == season
+      contestants.each do |att|
+        tot_age += att["age"].to_i
+        no_of_cont =+ 1
       end
     end
   end
-  average = age_total.to_f / num_of_cont.to_f
-  average.to_s[3].to_i >= 5 ? average.ceil : average.to_i
+
+  average = age.to_f / no_of_cont.to_f
+
 end
